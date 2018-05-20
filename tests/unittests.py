78da8ccd40853b1240b338d69139bece5a78baba
Saptarshi Sen <saptarshi.mrg@gmail.com>
2018-05-20 11:13:54 -0700

    Added unit tests for data validation
        +) Integrity test for copying a directory and then comparing the
           md5sum between original and copied for all files in the directory.

--- tests/unittests.py
+++ tests/unittests.py
@@ -8,18 +8,32 @@ import os
 import time
 import random
 import unittest
+import hashlib
+from shutil import copyfile
 
 from setup import RunCommand
 
 TESTDIR='/mnt'
-HOMEDIR='/home'
-SAMPLEFILE='core-sig-11'
+#HOMEDIR='/home'
+HOMEDIR="/run/media/ssen/NIKON D3200/DCIM/100D3200"
+#SAMPLEFILE='core-sig-11'
+SAMPLEFILE='DSC_0727.JPG'
 
 def GetSourceFile():
     return '{path}/{srcfile}'.format(path=HOMEDIR, srcfile=SAMPLEFILE)
 
 def GetTestFile():
     return '{path}/test-{id}'.format(path=TESTDIR, id=random.randint(1, 100))
+
+def GetMD5(filename):
+    m = hashlib.md5()
+    with open(filename, 'r') as f:
+        while True:
+            chunk = f.read(4096)
+            if chunk == '':
+                break
+            m.update(chunk)
+    return m.hexdigest()
 
 class LuciUnitTests(unittest.TestCase):
 
@@ -37,6 +51,7 @@ class LuciUnitTests(unittest.TestCase):
         """
         pass
 
+    @unittest.skip('skip test')
     def test_CopyFile(self):
         """
             Unit test for insert operations on arep tables
@@ -46,6 +61,7 @@ class LuciUnitTests(unittest.TestCase):
         rc = RunCommand(cmd)
         self.assertEqual(rc, 0)
 
+    @unittest.skip('skip test')
     def test_DDFile(self):
         """
             Unit test for insert operations on arep tables
@@ -57,6 +73,34 @@ class LuciUnitTests(unittest.TestCase):
              count=total_blocks)
         rc = RunCommand(cmd)
         self.assertEqual(rc, 0)
+
+    @unittest.skip('skip test')
+    def test_CopyDir(self):
+        """
+          Unit test to copy directory
+        """
+        for root, dirs, files in os.walk(HOMEDIR):
+            nr = len(files)
+            for filename in files:
+                srcFile = (os.path.join(root, filename))
+                dstFile = (os.path.join(TESTDIR, filename))
+                copyfile(srcFile, dstFile)
+                nr = nr - 1
+                print ("Pending files :" + str(nr))
+
+    #@unittest.skip('skip test')
+    def test_Validation(self):
+        """
+          Unit test to validate data by running md5 checksumming
+        """
+        for root, dirs, files in os.walk(HOMEDIR):
+            for filename in files:
+                srcFile = (os.path.join(root, filename))
+                srcMd5 = GetMD5(srcFile)
+                dstFile = (os.path.join(TESTDIR, filename))
+                dstMd5 = GetMD5(dstFile)
+                print ('{} {} : {}'.format(filename, srcMd5, dstMd5))
+                self.assertEqual(srcMd5, dstMd5)
 
 def TestDriver():
     suite = unittest.TestLoader().loadTestsFromTestCase(LuciUnitTests)
