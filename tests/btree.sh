0c2ef52ca49da95fc2bfcf1fc8de740011e21eba
Saptarshi Sen <saptarshi.mrg@gmail.com>
2019-03-01 09:45:59 -0800

    Left sibling search
    Signed-off-by: Saptarshi Sen <saptarshi.mrg@gmail.com>

--- tests/btree.sh
+++ tests/btree.sh
@@ -6,10 +6,12 @@ rmmod $MOD
 insmod lib/$MOD
 
 #for i in {1..1048576};
-for i in {0..8192};
+for i in {0..64};
         do 
-                echo $i > /sys/kernel/debug/btree/dump;
-                #echo "******BTREE LAYOUT********"
-                #cat /sys/kernel/debug/btree/dump;
+                key=$(($i * 2))
+                echo $key
+                echo $key > /sys/kernel/debug/btree/dump;
+                echo "******BTREE LAYOUT********"
+                cat /sys/kernel/debug/btree/dump;
                 #sleep 1;
         done
